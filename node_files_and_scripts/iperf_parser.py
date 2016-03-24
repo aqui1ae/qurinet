7a499bb7b2eab20d991790f7ac40af0de9616fa1
Charlie Le <chtle@ucdavis.edu>
2016-03-24 01:44:05 -0700

    Changing iperf for name, data spec

--- node_files_and_scripts/iperf_parser.py
+++ node_files_and_scripts/iperf_parser.py
@@ -4,24 +4,30 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 IPERF_DIRECTORY = SCRIPT_DIR + '/iperf'
-bandwidthDetail = []
 INDEX_FOR_LOCAL_NAME = 2
 INDEX_FOR_REMOTE_NAME = 1
 
-details = []
+bandwidthDetails = []
+data = []
 # Generate connection JSON objects from the iperf JSON output
 for iperf_file in os.listdir(IPERF_DIRECTORY):
     with open(IPERF_DIRECTORY + '/' + iperf_file) as jsonfile:
         jsonobj = json.loads(jsonfile.read())
         connection = jsonobj['start']['connected'][0]
+
+        bandwidthDetail = {}
+        bandwidthDetail['name'] = "{} -> {}".format(connection['local_host'], connection['remote_host'])
+        bandwidthDetail['data'] = data
+
         for interval in jsonobj['intervals']:
             summary = interval['sum']
-            details.append({
-                'site': int(connection['local_host'].split('.')[INDEX_FOR_LOCAL_NAME]),
-                'to': int(connection['remote_host'].split('.')[INDEX_FOR_REMOTE_NAME]),
+            data.append({
                 'bandwidth': float(summary['bits_per_second']),
-                'interval': '{}-{}'.format(summary['start'], summary['end'])
+                'interval': '{}'.format(summary['end'])
             })
+
+        bandwidthDetails.append(bandwidthDetail)
+
 print json.dumps({
-    'bandwidthDetail' : details
+    'bandwidthDetail' : bandwidthDetails
 })
