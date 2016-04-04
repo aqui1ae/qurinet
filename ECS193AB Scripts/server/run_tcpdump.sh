4a3313d12b0c77ed5dbe350820c73643a956f217
Courtney Laux <calaux23@gmail.com>
2016-04-03 23:24:44 -0700

    Added script to send tcpdump output to client machine

---
+++ ECS193AB Scripts/server/run_tcpdump.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# assumes script is in home directory
+# run with crontab
+# */5 * * * * /root/run_tcpdump.sh
+
+username='lc93-1'
+hostip='10.102.100.188'
+remotepath="/home/lc93-1/Classes/ECS193A/qurinet/ECS193AB\ Scripts/client/tcpdump"
+
+cd
+
+tcpdump -G 300 -W 1 -w tcpdump_$HOSTNAME.pcap -i adhoc0
+scp tcpdump_$HOSTNAME.pcap $username@$hostip:"${remotepath}"
